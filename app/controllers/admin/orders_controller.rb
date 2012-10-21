72a9f3eacfe1daf5fbebc7a39dee465382175a16
Jedrek Siema <jenczuen@gmail.com>
2012-10-21 16:23:49 +0200

    przegladanie zamowien

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,11 @@
 class Admin::OrdersController < Admin::AdminController
 	def index
-		@orders = Order.All	
+		@orders = Order.all	
 	end
+
+	def show
+		@order = Order.find(params[:id])	
+		@items = @order.order_items
+	end
+
 end
